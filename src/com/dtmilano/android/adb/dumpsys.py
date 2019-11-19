766bedae1bfdff4b3c8395be098e1a3cfdcdcab7
TiagoFraga <t.fraga94@hotmail.com>
2019-11-19 16:59:53 +0000

    add edited view client

---
+++ src/com/dtmilano/android/adb/dumpsys.py
@@ -0,0 +1,163 @@
+'''
+Copyright (C) 2012-2018  Diego Torres Milano
+Created on Dec 1, 2012
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: Diego Torres Milano
+'''
+from __future__ import print_function
+
+import re
+import sys
+from _warnings import warn
+
+__version__ = '15.8.1'
+
+DEBUG = False
+
+
+class Dumpsys:
+    FRAMESTATS = 'framestats'
+    GFXINFO = 'gfxinfo'
+    MEMINFO = 'meminfo'
+    RESET = 'reset'
+
+    ACTIVITIES = 'activities'
+    TOTAL = 'total'
+    VIEW_ROOT_IMPL = 'viewRootImpl'
+    VIEWS = 'views'
+
+    FLAGS = 0
+    INTENDED_VSYNC = 1
+    FRAME_COMPLETED = 13
+
+    def __init__(self, adbclient, subcommand, *args):
+        self.nativeHeap = -1
+        self.dalvikHeap = -1
+        self.total = 0
+        self.views = -1
+        self.activities = -1
+        self.appContexts = -1
+        self.viewRootImpl = -1
+        self.gfxProfileData = []
+        self.framestats = []
+        if args:
+            args_str = ' '.join(args)
+        else:
+            args_str = ''
+        if adbclient:
+            cmd = 'dumpsys ' + subcommand + (' ' + args_str if args_str else '')
+            self.parse(adbclient.shell(cmd), subcommand, *args)
+        else:
+            warn('No adbclient specified')
+
+    @staticmethod
+    def listSubCommands(adbclient):
+        return Dumpsys(adbclient, '-l')
+
+    @staticmethod
+    def meminfo(adbclient, args=None):
+        return Dumpsys(adbclient, Dumpsys.MEMINFO, args)
+
+    def get(self, name):
+        return getattr(self, name)
+
+    def parse(self, out, subcommand, *args):
+        if subcommand == Dumpsys.MEMINFO:
+            self.parseMeminfo(out)
+        elif subcommand == Dumpsys.GFXINFO:
+            if Dumpsys.RESET in args:
+                # Actually, reset does not need to parse anything
+                pass
+            elif Dumpsys.FRAMESTATS in args:
+                self.parseGfxinfoFramestats(out)
+            else:
+                self.parseGfxinfo(out)
+        elif '-l':
+            # list dumpsys subcommands
+            return out
+        else:
+            pass
+
+    def parseMeminfo(self, out):
+        m = re.search('Native Heap[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.nativeHeap = int(m.group(1))
+        m = re.search('Dalvik Heap[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.dalvikHeap = int(m.group(1))
+        m = re.search('Views:[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.views = int(m.group(1))
+        m = re.search('Activities:[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.activities = int(m.group(1))
+        m = re.search('AppContexts:[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.appContexts = int(m.group(1))
+        m = re.search('ViewRootImpl:[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.viewRootImpl = int(m.group(1))
+
+        m = re.search('TOTAL[ \t]*(\d+)', out, re.MULTILINE)
+        if m:
+            self.total = int(m.group(1))
+        else:
+            raise RuntimeError('Cannot find TOTAL in "' + out + '"')
+
+    def parseGfxinfo(self, out):
+        pass
+
+    def parseGfxinfoFramestats(self, out):
+        pd = '---PROFILEDATA---'
+        l = re.findall(r'%s.*?%s' % (pd, pd), out, re.DOTALL)
+        if l:
+            s = ''
+            for e in l:
+                if not e:
+                    continue
+                sl = e.splitlines()
+                for s in sl:
+                    if s == pd:
+                        continue
+                    pda = s.split(',')
+                    if pda[Dumpsys.FLAGS] == 'Flags':
+                        if pda[Dumpsys.INTENDED_VSYNC] != 'IntendedVsync' and pda[
+                                Dumpsys.FRAME_COMPLETED] != 'FrameCompleted':
+                            raise RuntimeError('Unsupported gfxinfo version')
+                        continue
+                    if pda[Dumpsys.FLAGS] == '0':
+                        # Only keep lines with Flags=0
+                        # If this is non-zero the row should be ignored, as the frame has been determined as being an
+                        # outlier from normal performance, where it is expected that layout & draw take longer than
+                        # 16ms.
+                        # See https://developer.android.com/training/testing/performance.html#timing-info for details
+                        # on format
+                        if DEBUG:
+                            print('pda={}'.format(pda), file=sys.stderr)
+                        self.gfxProfileData.append(pda[:-1])
+                        # All done! The total time spent working on this frame can be computed by doing
+                        # FRAME_COMPLETED - INTENDED_VSYNC.
+                        self.framestats.append(
+                            (int(pda[Dumpsys.FRAME_COMPLETED]) - int(pda[Dumpsys.INTENDED_VSYNC])) / 10 ** 6)
+        else:
+            raise RuntimeError('No profile data found')
+
+    @staticmethod
+    def gfxinfo(adbclient, *args):
+        return Dumpsys(adbclient, Dumpsys.GFXINFO, *args)
+
+    @staticmethod
+    def resetGfxinfo(adbclient, pkg):
+        return Dumpsys(adbclient, Dumpsys.GFXINFO, pkg, Dumpsys.RESET)
---
+++ src/com/dtmilano/android/plot.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2012-2018  Diego Torres Milano
+Created on mar 11, 2017
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: Diego Torres Milano
+
+"""
+from __future__ import print_function
+
+import sys
+import types
+from math import ceil
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.axisartist as AA
+import numpy as np
+from mpl_toolkits.axes_grid1 import host_subplot
+
+from com.dtmilano.android.adb.dumpsys import Dumpsys
+
+__version__ = '15.8.1'
+
+DEBUG = True
+
+NumberTypes = (types.IntType, types.LongType, types.FloatType)
+
+
+class Plot:
+    def __init__(self):
+        self.n = 0
+        self.na = []
+        self.va = []
+        self.ava = {}
+        ''' Associative values array '''
+        self.aava = {}
+        ''' (another) Associative values array '''
+
+    def append(self, value):
+        if DEBUG:
+            print('append({})'.format(value), file=sys.stderr)
+        self.n += 1
+        self.na.append(self.n)
+        if isinstance(value, NumberTypes):
+            self.va.append(value)
+        elif isinstance(value, Dumpsys):
+            if not self.ava:
+                self.__initAva()
+            if not self.aava:
+                self.__initAava()
+            dumpsys = value
+            self.ava[Dumpsys.TOTAL].append(dumpsys.get(Dumpsys.TOTAL))
+            self.ava[Dumpsys.ACTIVITIES].append(dumpsys.get(Dumpsys.ACTIVITIES))
+            self.ava[Dumpsys.VIEWS].append(dumpsys.get(Dumpsys.VIEWS))
+            # self.ava[Dumpsys.VIEW_ROOT_IMPL].append(dumpsys.get(Dumpsys.VIEW_ROOT_IMPL))
+            self.aava[Dumpsys.FRAMESTATS].append(dumpsys.get(Dumpsys.FRAMESTATS))
+        return self
+
+    def __initAva(self):
+        self.ava[Dumpsys.TOTAL] = []
+        self.ava[Dumpsys.ACTIVITIES] = []
+        self.ava[Dumpsys.VIEWS] = []
+        # self.ava[Dumpsys.VIEW_ROOT_IMPL] = []
+
+    def __initAava(self):
+        self.aava[Dumpsys.FRAMESTATS] = []
+
+    def plot(self, _type=Dumpsys.MEMINFO, filename=None):
+        title = "Dumpsys"
+        if _type == Dumpsys.FRAMESTATS:
+            subtitle = "gfxinfo " + Dumpsys.FRAMESTATS
+        else:
+            subtitle = _type
+        if _type == Dumpsys.MEMINFO:
+            if self.ava:
+                if DEBUG:
+                    print("plot:", file=sys.stderr)
+                    for k in self.ava.keys():
+                        print("   {}: {}".format(k, self.ava[k]), file=sys.stderr)
+
+                host = host_subplot(111, axes_class=AA.Axes)
+                plt.subplots_adjust(right=0.75)
+                par = {}
+                for k in self.ava.keys():
+                    if k != Dumpsys.TOTAL:
+                        par[k] = host.twinx()
+
+                axis = 1
+                for k in self.ava.keys():
+                    if k != Dumpsys.TOTAL and k != Dumpsys.ACTIVITIES:
+                        offset = axis * 60
+                        axis += 1
+                        new_fixed_axis = par[k].get_grid_helper().new_fixed_axis
+                        par[k].axis["right"] = new_fixed_axis(loc="right",
+                                                              axes=par[k],
+                                                              offset=(offset, 0))
+                        par[k].axis["right"].toggle(all=True)
+
+                if DEBUG:
+                    print("setting host x lim {} {}".format(np.amin(self.na), np.amax(self.na)), file=sys.stderr)
+                minx = np.amin(self.na)
+                maxx = np.amax(self.na)
+                divx = abs(maxx - minx) / (len(self.na) * 1.0)
+                host.set_xlim(minx - divx, maxx + divx)
+                miny = np.amin(self.ava[Dumpsys.TOTAL])
+                maxy = np.amax(self.ava[Dumpsys.TOTAL])
+                divy = ceil(abs(maxy - miny) / (len(self.ava[Dumpsys.TOTAL]) * 1.0))
+                if DEBUG:
+                    print("setting host y lim {} {}".format(miny - divy, maxy + divy), file=sys.stderr)
+                host.set_ylim(miny - divy, maxy + divy)
+                host.set_xlabel('N')
+                host.set_ylabel(Dumpsys.TOTAL)
+
+                for k in self.ava.keys():
+                    if k != Dumpsys.TOTAL:
+                        par[k].set_ylabel(k)
+
+                plots = {}
+                if DEBUG:
+                    print("    host plot {} : {}".format(self.na, self.ava[Dumpsys.TOTAL]), file=sys.stderr)
+                plots[Dumpsys.TOTAL], = host.plot(self.na, self.ava[Dumpsys.TOTAL], label=Dumpsys.TOTAL, linewidth=2)
+                for k in self.ava.keys():
+                    if k != Dumpsys.TOTAL:
+                        if DEBUG:
+                            print("   {} plot {} : {}".format(k, self.na, self.ava[k]), file=sys.stderr)
+                        plots[k], = par[k].plot(self.na, self.ava[k], label=k, linewidth=2)
+
+                for k in self.ava.keys():
+                    if k != Dumpsys.TOTAL:
+                        miny = np.amin(self.ava[k])
+                        maxy = np.amax(self.ava[k])
+                        divy = ceil(abs(maxy - miny) / (len(self.ava[k]) * 1.0))
+                        if DEBUG:
+                            print("setting {} y lim {}".format(k ,(miny - divy, maxy + divy)), file=sys.stderr)
+                        par[k].set_ylim(miny - divy, maxy + divy)
+
+                host.legend()
+
+                # host.axis["left"].label.set_color(plots[Dumpsys.TOTAL].get_color())
+                # for k in self.ava.keys():
+                #     if k != Dumpsys.TOTAL:
+                #         par[k].axis["right"].label.set_color(plots[k].get_color())
+
+            elif self.va:
+                plt.xlabel('N')
+                plt.ylabel('V')
+                plt.plot(self.na, self.va, label="A")
+            else:
+                raise RuntimeError("No values to plot")
+        elif _type == Dumpsys.FRAMESTATS:
+            if DEBUG:
+                print("    plot: histogram {}".format(self.aava[Dumpsys.FRAMESTATS]), file=sys.stderr)
+            n, bins, patches = plt.hist(self.aava[Dumpsys.FRAMESTATS])
+            ymax = np.amax(n)
+            x = []
+            y = []
+            for v in range(int(ceil(ymax)) + 1):
+                x.append(1 / 60.0 * 10 ** 3)
+                y.append(v)
+            plt.plot(x, y, linewidth=2, color='c')
+            x = []
+            y = []
+            for v in range(int(ceil(ymax)) + 1):
+                x.append(1 / 30.0 * 10 ** 3)
+                y.append(v)
+            plt.plot(x, y, linewidth=2, color='r')
+            plt.xlabel('ms')
+            plt.ylabel('Frames')
+
+        plt.title(title + ' ' + subtitle)
+        plt.grid(True)
+        plt.draw()
+        if filename:
+            plt.savefig(filename)
+        else:
+            plt.show()
